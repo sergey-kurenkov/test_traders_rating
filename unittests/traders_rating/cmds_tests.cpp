fd8f812e4ca378d546163749faf0ae6c0f80dfd3
Sergey.Kurenkov <skwllsp@gmail.com>
2017-09-23 21:47:37 +0100

    compilation error

--- unittests/traders_rating/cmds_tests.cpp
+++ unittests/traders_rating/cmds_tests.cpp
@@ -14,11 +14,11 @@ struct user_registered_test : public ::testing::Test {
 		test_callback_ = std::bind(&user_registered_test::test_callback, this, _1, _2);
 	}
 
-	void test_callback(user_id_t, user_name_t) {
+	void test_callback(tr::user_id_t, tr::user_name_t) {
 
 	}
 
-	user_registered_callback test_callback_;
+	tr::user_registered_callback test_callback_;
 };
 
 TEST(user_registered_test, constuctor) {
