6a3e93a004cd741dede89e8d1aa6a4460ea6d83b
Sergey.Kurenkov <skwllsp@gmail.com>
2017-09-23 21:50:20 +0100

    compilation error

--- unittests/traders_rating/cmds_tests.cpp
+++ unittests/traders_rating/cmds_tests.cpp
@@ -22,5 +22,5 @@ struct user_registered_test : public ::testing::Test {
 };
 
 TEST(user_registered_test, constuctor) {
-	cmd = tr::user_registered_test(1, "test", test_callback_);
+	tr::user_registered_test cmd(1, "test", test_callback_);
 }
