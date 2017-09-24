fba22d0acf3649626f65fbff63a512dde88f1e13
Sergey.Kurenkov <skwllsp@gmail.com>
2017-09-24 08:36:21 +0100

    + unittest

--- unittests/traders_rating/cmds_tests.cpp
+++ unittests/traders_rating/cmds_tests.cpp
@@ -14,7 +14,7 @@ struct UserRegisteredTest : public ::testing::Test {
 		test_callback_ = std::bind(&UserRegisteredTest::test_callback, this, _1, _2);
 	}
 
-	void test_callback(tr::user_id_t id, tr::user_name_t name) {
+	void test_callback(tr::user_id_t id, const tr::user_name_t& name) {
 		called++;
 		user_id = id;
 		user_name = name;
@@ -42,6 +42,28 @@ TEST_F(UserRegisteredTest, Callback) {
 		ASSERT_EQ(called, 1);
 		ASSERT_EQ(user_id, 10);
 		ASSERT_STREQ(user_name.c_str(), "test11");
+	} catch(std::exception& e) {
+		FAIL() << e.what();
+	}
+}
+
+
+TEST(UserRenameTest, Callback) {
+	try {
+		tr::user_id_t test_id = 0;
+		tr::user_name_t test_name = "";
+		unsigned called = 0;
+
+		tr::cmd_uptr cmd(new tr::user_renamed(20, "test12", 
+			[&](tr::user_id_t id, const tr::user_name_t& name){
+				test_id = id;
+				test_name = name;
+				++called;
+			}));
+		cmd->handle();
+		ASSERT_EQ(called, 1);
+		ASSERT_EQ(user_id, 20);
+		ASSERT_STREQ(user_name.c_str(), "test12");
 	} catch(std::exception& e) {
 		FAIL() << e.what();
 	}
