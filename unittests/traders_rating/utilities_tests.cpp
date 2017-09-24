b8cb7d80c239327b5483e5e829684ded2ee4f1eb
Sergey.Kurenkov <skwllsp@gmail.com>
2017-09-24 10:13:32 +0100

    get_week_time unittest

--- unittests/traders_rating/utilities_tests.cpp
+++ unittests/traders_rating/utilities_tests.cpp
@@ -18,3 +18,15 @@ TEST(GetMinuteTimeTest, Test1)  {
 		FAIL() << e.what();
 	}
 }
+
+TEST(GetWeekTimeTest, Test1)  {
+	try {
+		time_t ts = time(nullptr);
+		tm ts_tm = *localtime(&ts);
+		auto curr_day = ts_tm.tm_wday == 0 ? 6 : ts_tm.tm_wday - 1;
+		auto res = tr::get_week_times(ts);
+		ASSERT_EQ(res.first, ts - ts_tm.tm_sec - ts_tm.tm_min*60 - ts_tm.tm_hour*3600 - curr_day * 24 * 3600);
+	} catch(std::exception& e) {
+		FAIL() << e.what();
+	}
+}
